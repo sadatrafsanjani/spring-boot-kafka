b176699e6c860d85f24a84acc7dfa1a5019b9af1
sadatrafsanjani <sadatrafsanjani@gmail.com>
2022-10-25 02:29:22 +0600

    synchronous polling and acknowledge

--- src/main/java/com/example/kafkademo/config/KafkaConsumerConfig.java
+++ src/main/java/com/example/kafkademo/config/KafkaConsumerConfig.java
@@ -1,6 +1,7 @@
 package com.example.kafkademo.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +11,7 @@ import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.listener.ContainerProperties;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,8 +27,9 @@ public class KafkaConsumerConfig {
         Map<String, Object> properties = new HashMap<>();
 
         properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
 
         return properties;
     }
@@ -38,11 +41,12 @@ public class KafkaConsumerConfig {
     }
 
 
-    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, String>> listenerFactory(
-            ConsumerFactory<String, String> consumerFactory){
+    @Bean
+    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, String>> listenerFactory(ConsumerFactory<String, String> consumerFactory){
 
         ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory);
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
 
         return factory;
     }
--- src/main/java/com/example/kafkademo/controller/ProducerController.java
+++ src/main/java/com/example/kafkademo/controller/ProducerController.java
@@ -1,38 +1,76 @@
 package com.example.kafkademo.controller;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Random;
 
 @Slf4j
 @RestController
 @RequestMapping("/api")
+@AllArgsConstructor
 public class ProducerController {
 
     private final KafkaTemplate<String , String> kafkaTemplate;
 
-    @Autowired
-    public ProducerController(KafkaTemplate<String, String> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
-    }
-
     @GetMapping
     public String producer(){
 
-        for (int i=1; i< 11; i++){
-            kafkaTemplate.send("aster", "Hello World *_*");
+        for(int i=1; i<=21; i++ ){
+            Random random = new Random();
+            int number = random.nextInt(200 - 1) + 1;
+            kafkaTemplate.send("aster", String.valueOf(number));
+            log.info("Want " + number + "nth Prime");
+
+            if((i % 5) == 0){
+                try{
+                    Thread.sleep(2000);
+                }
+                catch (Exception e){}
+            }
         }
 
         return "Message published...";
     }
 
     @KafkaListener(topics = "aster", groupId = "groupId")
-    public void consumer(String data){
+    public void consumer(@Payload String payload, Acknowledgment acknowledgment){
 
-        System.out.println(data + " ......... ");
+        int number = getNthPrime(Integer.parseInt(payload));
+
+        if(number != 0){
+            log.info(payload + "nth Prime = " + number);
+            acknowledgment.acknowledge();
+        }
+    }
+
+    private int getNthPrime(int nth){
+
+        log.info("Calculating " + nth + "nth Prime");
+
+        int num, count, i;
+        num=1;
+        count=0;
+
+        while (count < nth){
+            num=num+1;
+            for (i = 2; i <= num; i++){
+                if (num % i == 0) {
+                    break;
+                }
+            }
+            if ( i == num){
+                count++;
+            }
+        }
+
+        return num;
     }
 }
